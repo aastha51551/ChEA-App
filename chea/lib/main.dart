7922ea8e590c5166bc03be06225415fec292bbf8
Manish <msn07082005@gmail.com>
2024-06-07 18:01:42 +0530

    Add: montserrat font -pubspec.yaml Edit : Changed opportunities carousel to be dynamic with size

--- chea/lib/main.dart
+++ chea/lib/main.dart
@@ -2,27 +2,47 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+int defaultBackground = 0xff08050c;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    const List<Map<String, String>> Opportunities = [
+      {
+        'title': 'Internship',
+        'provider': 'BASF, GER',
+        'timePosted': '2 days ago'
+      },
+      {
+        'title': 'Project',
+        'provider': 'Prof. Rochish',
+        'timePosted': '5 days ago'
+      },
+      {'title': 'Internship', 'provider': 'Shell', 'timePosted': '1 week ago'}
+    ];
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'ChEA',
         home: Scaffold(
-          backgroundColor: Colors.black,
+          backgroundColor: Color(defaultBackground),
           appBar: AppBar(
             toolbarHeight: 100,
-            backgroundColor: Colors.black,
+            backgroundColor: Color(defaultBackground),
             elevation: 0,
             leading: SvgPicture.asset(
               'assets/icons/logo.svg',
@@ -35,11 +55,11 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              _firstCarousel(), //Opportunitites Section
+              opportunities(Opportunities), //Opportunitites Section
               //What's Happening Section
               Container(
                   width: width,
-                  color: Colors.black,
+                  color: Color(defaultBackground),
                   child: const Column(children: [
                     Padding(
                       padding: EdgeInsets.all(15.0),
@@ -56,8 +76,8 @@ class MyApp extends StatelessWidget {
                     )
                   ])),
               Container(
-                color: Colors.black,
-                height: height * 0.4,
+                color: Color(defaultBackground),
+                height: height * 0.3,
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
@@ -158,188 +178,134 @@ class MyApp extends StatelessWidget {
               )),
         ));
   }
+}
 
-  Container _firstCarousel() {
-    return Container(
-      color: Colors.black,
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 15.0),
-            child: Text(
-              'YOUR OPPORTUNITITES',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.w700),
-            ),
+Container opportunities(List<Map<String, String>> Opportunities) {
+  return Container(
+    color: Color(defaultBackground),
+    child: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 15.0),
+          child: Text(
+            'YOUR OPPORTUNITITES',
+            style: TextStyle(
+                color: Colors.white, fontSize: 28, fontWeight: FontWeight.w700),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 150,
-            child: ListView(
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Center(
+          child: SizedBox(
+            height: 185,
+            width: double.infinity,
+            child: ListView.builder(
               shrinkWrap: true,
+              itemCount: Opportunities.length,
               scrollDirection: Axis.horizontal,
-              children: [
-                const SizedBox(
-                  width: 20,
-                ),
-                Container(
-                  height: 150,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey[900]),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 8.0, right: 8.0, top: 8.0, bottom: 5.0),
-                        child: Text(
-                          'Internship',
-                          style: TextStyle(
-                              color: Colors.grey[400],
-                              fontSize: 20,
-                              fontWeight: FontWeight.w800,
-                              fontStyle: FontStyle.italic),
-                        ),
-                      ),
-                      const Text(
-                        'BASF, GER',
-                        style: TextStyle(
-                            color: Colors.white, fontStyle: FontStyle.italic),
-                      ),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.watch_later_outlined,
-                            color: Colors.white,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            '2 days ago',
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Expanded(
-                        child: Container(
-                          width: 250,
-                          decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              color: Colors.grey[400]),
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Text('CHECK STATUS',
-                                  style: TextStyle(
-                                      color: Colors.grey[900],
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w800)),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Container(
-                  height: 150,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey[900]),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 8.0, right: 8.0, top: 8.0, bottom: 5.0),
-                        child: Text(
-                          'Project',
-                          style: TextStyle(
-                              color: Colors.grey[400],
-                              fontSize: 20,
-                              fontWeight: FontWeight.w800,
-                              fontStyle: FontStyle.italic),
-                        ),
-                      ),
-                      const Text(
-                        'Prof.Rochish',
-                        style: TextStyle(
-                            color: Colors.white, fontStyle: FontStyle.italic),
-                      ),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.watch_later_outlined,
-                            color: Colors.white,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            '5 days ago',
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Expanded(
-                        child: Container(
-                          width: 250,
-                          decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              color: Colors.grey[400]),
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Text('CHECK STATUS',
-                                  style: TextStyle(
-                                      color: Colors.grey[900],
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w800)),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Container(
-                  height: 150,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey[900]),
-                ),
-              ],
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: _opportunities(Opportunities[index]),
+                );
+              },
             ),
-          )
-        ],
-      ),
-    );
-  }
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _opportunities(Map<String, String> Opportunitiy) {
+  return Container(
+    width: 225,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40), color: Color(0xff201F1F)),
+    child: Column(
+      children: [
+        SizedBox(
+          width: 225,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 20.0, right: 8.0, top: 15.0, bottom: 0.0),
+            child: Text(
+              Opportunitiy['title'] ?? 'Not Defined',
+              // style: TextStyle(
+
+              //     color: Colors.grey[400],
+              //     fontSize: 20,
+              //     fontWeight: FontWeight.w800,
+              //     fontStyle: FontStyle.italic),
+              style: GoogleFonts.montserrat(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                  color: Color(0xff666161)),
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ),
+        Container(
+          width: 225,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Text(
+              Opportunitiy['provider'] ?? 'Not Defined',
+              style: GoogleFonts.montserrat(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xffffffff)),
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.watch_later_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                Opportunitiy['timePosted'] ?? 'Not Defined',
+                style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    fontStyle: FontStyle.italic,
+                    color: Color(0xffffffff)),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 20),
+        Container(
+            width: 225,
+            height: 50,
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40)),
+                color: Color(0xff403c3c)),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 5),
+                child: Text('CHECK STATUS',
+                    style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xffb3a9a9),
+                      fontSize: 14,
+                    )),
+              ),
+            ))
+      ],
+    ),
+  );
 }
